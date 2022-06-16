ce822e9f0557ed3e34f3b0e1f82fa72566f2f33a
amin torabi masoumi <Torabi.dsd@gmail.com>
2022-06-17 00:35:05 +0430

    creating feedLoader Protocol and FeedItem

---
+++ Essential Feed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Essential Feed
+//
+//  Created by Amin on 3/27/1401 AP.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
