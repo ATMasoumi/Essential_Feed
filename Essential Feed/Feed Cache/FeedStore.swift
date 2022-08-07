afdcd4a8c993e3cad3f43cf8ca190d070691ed74
amin torabi masoumi <Torabi.dsd@gmail.com>
2022-08-07 17:25:20 +0430

    Move 'FeedStore' to its own file

---
+++ Essential Feed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  Essential Feed
+//
+//  Created by Amin on 5/16/1401 AP.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
