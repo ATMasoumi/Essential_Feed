539c7d5b3359b988adf8ba220068a04095694c8c
amin torabi masoumi <Torabi.dsd@gmail.com>
2022-08-10 22:02:49 +0430

    Load command deletes seven days old cache upon retrieval

--- Essential Feed/Feed Cache/LocalFeedLoader.swift
+++ Essential Feed/Feed Cache/LocalFeedLoader.swift
@@ -39,7 +39,10 @@ public class LocalFeedLoader {
             case let .failure(error):
                 store.deleteCachedFeed { _ in }
                 completion(.failure(error))
-            case .found, .empty:
+            case .found:
+                store.deleteCachedFeed { _ in }
+                completion(.success([]))
+            case .empty:
                 completion(.success([]))
             }
         }
--- Essential FeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ Essential FeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -106,6 +106,20 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
+    func test_load_deleteCacheOnSevenDaysOldCache() {
+      
+        let feed = uniqueImageFeed()
+        let (sut, store) = makeSUT()
+        let fixedCurrentDate = Date()
+        let sevenDaysOldTimeStamp = fixedCurrentDate.adding(days: -7)
+        
+        sut.load { _ in }
+        
+        store.completeRetrieval(with: feed.local, timestamp: sevenDaysOldTimeStamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeedMessage])
+    }
+    
     // MARK: Helper Methods
    
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
