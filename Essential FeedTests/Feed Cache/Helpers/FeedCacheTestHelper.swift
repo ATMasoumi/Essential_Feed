11f997dc8630ef205fa76ed63faf775d52d3e345
amin torabi masoumi <Torabi.dsd@gmail.com>
2022-08-11 23:41:19 +0430

    Separate 'Date' extension helpers in to distance contexts to clarify their scope (one is cache-policy specific DSL and the other is just a reusable  DSL helper )

--- Essential FeedTests/Feed Cache/Helpers/FeedCacheTestHelper.swift
+++ Essential FeedTests/Feed Cache/Helpers/FeedCacheTestHelper.swift
@@ -26,10 +26,11 @@ extension Date {
         return 7
     }
     
-    func adding(days: Int) -> Date {
+    private func adding(days: Int) -> Date {
         Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
-    
+}
+extension Date {
     func adding(seconds: TimeInterval) -> Date {
         self + seconds
     }
