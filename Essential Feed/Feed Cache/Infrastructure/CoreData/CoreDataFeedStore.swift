8aa934c8a1a6b91e193d7ad6beeea37bf39b23fc
amin torabi masoumi <Torabi.dsd@gmail.com>
2022-08-21 21:24:44 +0430

    Extract reusable CoreData helpers into a separate file

--- Essential Feed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ Essential Feed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -16,7 +16,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        perform { context in 
+        perform { context in
             do {
                 if let cache = try ManagedCache.find(in: context) {
                     completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
@@ -61,37 +61,6 @@ public final class CoreDataFeedStore: FeedStore {
         context.perform { action(context) }
     }
     
-}
-
-private extension NSPersistentContainer {
-    enum LoadingError: Swift.Error {
-        case modelNotFound
-        case failedToLoadPersistentStores(Swift.Error)
-    }
-    
-    static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
-        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
-            throw LoadingError.modelNotFound
-        }
-        
-        let description = NSPersistentStoreDescription(url: url)
-        let container = NSPersistentContainer(name: name, managedObjectModel: model)
-        container.persistentStoreDescriptions = [description]
-        
-        var loadError: Swift.Error?
-        container.loadPersistentStores { loadError = $1 }
-        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
-        
-        return container
-    }
-}
-
-private extension NSManagedObjectModel {
-    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
-        return bundle
-            .url(forResource: name, withExtension: "momd")
-            .flatMap { NSManagedObjectModel(contentsOf: $0) }
-    }
 }
 
 @objc(ManagedCache)
