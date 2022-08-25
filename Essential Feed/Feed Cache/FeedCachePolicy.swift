4ec251847eeaaf85135e627365ad9fd7fa9e3c6b
amin torabi masoumi <Torabi.dsd@gmail.com>
2022-08-25 13:58:35 +0430

    Remove redundant internal access control declarations

--- Essential Feed/Feed Cache/FeedCachePolicy.swift
+++ Essential Feed/Feed Cache/FeedCachePolicy.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-internal final class FeedCachePolicy {
+ final class FeedCachePolicy {
     private static let calendar = Calendar(identifier: .gregorian)
     private static var maxCacheInDays: Int {
         7
     }
-    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+     static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheInDays, to: timestamp) else {
             return false
         }
