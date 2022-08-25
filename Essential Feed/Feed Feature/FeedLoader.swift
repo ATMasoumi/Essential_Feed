0c24d0d2f554128610ed9a606ca20f0fec7bb3d2
amin torabi masoumi <Torabi.dsd@gmail.com>
2022-08-25 14:17:52 +0430

    Nest 'LoadFeedResult' into the 'FeedLoader' protocol as 'FeedLoader.Result' since they're closely related

--- Essential Feed/Feed Feature/FeedLoader.swift
+++ Essential Feed/Feed Feature/FeedLoader.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public typealias LoadFeedResult = Result<[FeedImage], Error>
 public protocol FeedLoader {
-    func load(completion:@escaping(LoadFeedResult) -> ())
+    typealias Result = Swift.Result<[FeedImage], Error>
+    func load(completion:@escaping(Result) -> ())
 }
--- Essential_FeedAPIEndToEndTests/Essential_FeedAPIEndToEndTests.swift
+++ Essential_FeedAPIEndToEndTests/Essential_FeedAPIEndToEndTests.swift
@@ -32,7 +32,7 @@ class Essential_FeedAPIEndToEndTests: XCTestCase {
         }
     }
     // MARK: helper methods
-    func getFeedResult( file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
+    func getFeedResult( file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
         let testServerURL = URL(string: "https://essentialDeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
@@ -40,7 +40,7 @@ class Essential_FeedAPIEndToEndTests: XCTestCase {
         trackForMemoryLeaks(loader,file: file, line: line)
         
         let exp = expectation(description: "wait for load completion")
-        var receivedResult: LoadFeedResult?
+        var receivedResult: FeedLoader.Result?
         loader.load { result in
             receivedResult = result
             exp.fulfill()
