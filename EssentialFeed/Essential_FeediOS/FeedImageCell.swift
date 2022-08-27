0ebdc8e4367e3e19035554e9ae6c44dee874ef28
amin torabi masoumi <Torabi.dsd@gmail.com>
2022-08-27 22:45:26 +0430

    render loaded feed on successful load completion

---
+++ EssentialFeed/Essential_FeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  Essential_FeediOS
+//
+//  Created by Amin on 6/5/1401 AP.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell  {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
