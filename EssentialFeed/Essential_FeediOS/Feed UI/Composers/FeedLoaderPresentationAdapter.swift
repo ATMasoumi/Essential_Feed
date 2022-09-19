0e324f98b5cf655bc65de74854d2cd497419cf67
amin torabi masoumi <Torabi.dsd@gmail.com>
2022-09-19 07:51:34 +0430

    Unify `FeedViewController` static factory helper into the `FeedUIComposer`

---
+++ EssentialFeed/Essential_FeediOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,31 @@
+//
+//  FeedLoaderPresentationAdapter.swift
+//  Essential_FeediOS
+//
+//  Created by Amin on 6/28/1401 AP.
+//
+
+import Essential_Feed
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    
+    private let feedLoader: FeedLoader
+    var presenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        presenter?.didStartLoadingFeed()
+        
+        feedLoader.load { [weak self] result in
+            switch result {
+            case let .success(feed):
+                self?.presenter?.didFinishLoadingFeed(with: feed)
+            case let .failure(error):
+                self?.presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}
