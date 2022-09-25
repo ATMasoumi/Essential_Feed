e2495370a44782d817c8302e1cafb46e93c119bf
amin torabi masoumi <Torabi.dsd@gmail.com>
2022-09-25 09:52:03 +0330

    Move `LocalFeedImageDataLoader` and `FeedImageDataStore` to production

---
+++ EssentialFeed/Essential Feed/Feed Cache/FeedImageDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  FeedImageDataSource.swift
+//  Essential Feed
+//
+//  Created by Amin on 7/3/1401 AP.
+//
+
+import Foundation
+
+public protocol FeedImageDataStore {
+    typealias Result = Swift.Result<Data?, Error>
+    func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
+}
